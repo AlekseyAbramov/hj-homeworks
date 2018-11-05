e4527648f41adb5a3d5f382583a51957f09de0e6
Aleksey Abramov <capiton2008@gmail.com>
2018-11-05 17:14:26 +0300

    исправление ошибок к занятию "Способы поиска нужного HTML-элемента"

--- html-element-collection/dropdown-menu/js/index.js
+++ html-element-collection/dropdown-menu/js/index.js
@@ -1,5 +1,4 @@
 const wrapperDropdown = document.getElementsByClassName('wrapper-dropdown')[0];
-const li = document.getElementsByTagName('li');
 
 wrapperDropdown.onclick = () => {
     wrapperDropdown.classList.toggle('active');
