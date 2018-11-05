6d1dbd3c6c47d1bbdaaf56561c10c3af0e127711
Aleksey Abramov <capiton2008@gmail.com>
2018-11-05 17:11:31 +0300

    исправление ошибок к занятию "Способы поиска нужного HTML-элемента"

--- html-element-collection/drum-machine/js/index.js
+++ html-element-collection/drum-machine/js/index.js
@@ -3,6 +3,8 @@ const musik =document.getElementsByTagName('audio');
 for (let drum of drums) {
     drum.onclick = () => {
         let sound = drum.getElementsByTagName('audio')[0];
+        sound.pause();
+        sound.currentTime = 0;
         sound.play();
     }
 };
