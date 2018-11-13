030dab75fd03111bba7d341418dac69e7ec0866c
Aleksey Abramov <capiton2008@gmail.com>
2018-11-13 19:16:30 +0300

    ДЗ к занятию «Принцип обработки событий»

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,7 @@
 'use strict';
+document.addEventListener('click', (event) => {
+	if (event.target.classList.contains('add-to-cart')) {
+		event.preventDefault();
+		addToCart(event.target.dataset);
+	}
+});
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.classList.contains('prop__name')) {
+    if (!event.target.dataset.dir) {
+        event.target.dataset.dir = 1;
+    } else {
+        if (event.target.dataset.dir == 1) {
+            event.target.dataset.dir = -1;
+        } else {
+            event.target.dataset.dir = 1;
+        }
+    }
+    document.querySelector('table').dataset.sortBy = event.target.dataset.propName;
+    sortTable(document.querySelector('table').dataset.sortBy,event.target.dataset.dir);
+  }
 }
